1c0af7b2a5b08fefaaa6c15bd48fd058aed78e6e
ilubov <o@ilubov.cn>
2020-08-30 00:14:45 +0800

    update

--- src/main/java/com/i/lubov/util/EventUtils.java
+++ src/main/java/com/i/lubov/util/EventUtils.java
@@ -1,11 +1,27 @@
+/*
+ * Copyright (c) 2020 ilubov
+ * https://www.ilubov.cn
+ * All rights reserved.
+ */
 package com.i.lubov.util;
 
 import com.google.common.eventbus.AsyncEventBus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public final class EventUtils {
-    public EventUtils() {}
+
+    @Autowired
+    private AsyncEventBus asyncEventBus;
+
+    private static EventUtils instance = null;
+
+    public EventUtils() {
+        instance = this;
+    }
 
     public static void postEvent(Object event) {
-        SpringContextUtils.getBean(AsyncEventBus.class).post(event);
+        instance.asyncEventBus.post(event);
     }
 }
